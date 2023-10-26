1c6a016627439df996533c338ed3c7a45e62d0f0
Tassio <tassions@hotmail.com>
2023-10-26 19:38:27 -0300

    feat: add createOrder server action

--- src/components/ui/cart.tsx
+++ src/components/ui/cart.tsx
@@ -9,15 +9,28 @@ import { ScrollArea } from "./scroll-area";
 import { Button } from "./button";
 import { createCheckout } from "@/actions/checkout";
 import { loadStripe } from "@stripe/stripe-js";
+import { createOrder } from "@/actions/order";
+import { useSession } from "next-auth/react";
 
 const Cart = () => {
+  const { data } = useSession();
+
   const { products, subtotal, total, totalDiscount } = useContext(CartContext);
 
   const handleFinishPurchaseClick = async () => {
-    const checkout = await createCheckout(products);
+    if (!data?.user) {
+      // TODO: redirecionar para o login
+      return;
+    }
+
+    const order = await createOrder(products, (data?.user as any).id);
+
+    const checkout = await createCheckout(products, order.id);
 
     const stripe = await loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY);
-    console.log(stripe)
+
+    // Criar pedido no banco
+
     stripe?.redirectToCheckout({
       sessionId: checkout.id,
     });

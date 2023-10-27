7123f150c3652e57c86aea3fa8392f3cf28e80a5
Tassio <tassions@hotmail.com>
2023-10-27 09:42:09 -0300

    feat:ensure order status is propery format

--- src/app/orders/components/order-item.tsx
+++ src/app/orders/components/order-item.tsx
@@ -11,7 +11,7 @@ import {
   import { Separator } from "@/components/ui/separator";
   import { useMemo } from "react";
   import { computeProductTotalPrice } from "@/helpers/product";
-  import { getOrderStatus } from "../helpers/status";
+  import { getOrderStatus } from "./helpers/status";
   
   interface OrderItemProps {
     order: Prisma.OrderGetPayload<{ // neste caso sera pra pegar o order com o join
